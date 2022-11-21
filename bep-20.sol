8ddba54ced5801c8349c8d4696dc63d0d662f00f
Kiril Petkov <49308732+KirilPetkov@users.noreply.github.com>
2022-11-21 09:51:28 +0000

    Create bep-20.sol
    bep-20.sol blank

---
+++ bep-20.sol
@@ -0,0 +1,11 @@
+/*
+pragma solidity ^0.8.0; <-- This is Solidity
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol"; <-- This is Contract
+
+contract SellRoma is ERC20 { <-- This is Name
+    constructor(uint256 initialSupply) public ERC20 ("SellRoma", "SLRMA"){ <-- This is Name
+        _mint(msg.sender,initialSupply);
+    }
+}
+*/
